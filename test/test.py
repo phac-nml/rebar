d469108a90e4e227a91dbed67320280683cc20bd
Katherine Eaton <katherine.eaton@phac-aspc.gc.ca>
2023-04-25 14:27:50 -0500

    test: capture and return subprocess code

--- test/test.py
+++ test/test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-import os
+import subprocess
+import sys
 
-os.system(
-    """
-    python -m coverage run -m pytest --cov=rebar --cov-report=html --cov-report=xml \
-        test/test_utils.py \
-        test/test_wrappers.py
-    """
+cmd_str = (
+    "python -m coverage run -m pytest --cov=rebar --cov-report=html --cov-report=xml"
+    " test/test_utils.py"
+    " test/test_wrappers.py"
 )
+result = subprocess.run(cmd_str, shell=True)
+# I'm not 100% sure this is necessary to pass the subprocess return code
+# mostly, I want CI to properly fail if a test failed
+sys.exit(result.returncode)
