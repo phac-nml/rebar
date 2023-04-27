4b0cf0bc2664513c4409b0acfb08ddeb33821b70
Katherine Eaton <katherine.eaton@phac-aspc.gc.ca>
2023-04-27 13:46:33 -0500

    plot: comment out testing commands

--- rebar/plot.py
+++ rebar/plot.py
@@ -5,7 +5,6 @@ import logging
 
 import matplotlib.pyplot as plt
 from matplotlib import patches, colors
-import pandas as pd
 
 logging.getLogger("matplotlib.font_manager").disabled = True
 
@@ -397,24 +396,3 @@ def plot(barcodes_df, summary_df, annot_df, output):
 
     # Close for memory management
     plt.close()
-
-
-annot_df = pd.read_csv("dataset/sars-cov-2-latest/annotations.tsv", sep="\t")
-
-barcodes_df = pd.read_csv("output/XBC/barcodes/XBC_CJ.1_B.1.617.2.tsv", sep="\t")
-summary_df = pd.read_csv("output/XBC/summary.tsv", sep="\t")
-plot(
-    barcodes_df=barcodes_df,
-    summary_df=summary_df,
-    annot_df=annot_df,
-    output="output/XBC/plots/XBC_CJ.1_B.1.617.2.png",
-)
-
-barcodes_df = pd.read_csv("output/XBJ/barcodes/XBJ_BA.2.3.20_BA.5.2.tsv", sep="\t")
-summary_df = pd.read_csv("output/XBJ/summary.tsv", sep="\t")
-plot(
-    barcodes_df=barcodes_df,
-    summary_df=summary_df,
-    annot_df=annot_df,
-    output="output/XBJ/plots/XBJ_BA.2.3.20_BA.5.2.png",
-)
