cea3f81164a1e12a10d79852c97fd93b37e16ac8
Katherine Eaton <katherine.eaton@phac-aspc.gc.ca>
2023-05-02 12:27:52 -0500

    test: wrappers prepare for testing missing params

--- test/test_wrappers.py
+++ test/test_wrappers.py
@@ -23,3 +23,15 @@ def test_run_sarscov2_latest(params):
     params.logger = utils.create_logger(os.path.join(params.outdir, "test.log"))
 
     wrappers.run(params)
+
+
+# def test_no_lineages_or_alignment(params):
+#     """Test function wrappers.run with missing lineages and alignment."""
+#     params.outdir = "test/tmp/wrappers/no_lineages_or_alignment"
+#     if not os.path.exists(params.outdir):
+#         os.makedirs(params.outdir)
+#     params.logger = utils.create_logger(os.path.join(params.outdir, "test.log"))
+
+#     params.alignment = None
+#     params.lineages = None
+#     wrappers.run(params)
