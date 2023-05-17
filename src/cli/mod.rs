d83e17a1e960795cd053a4ca985f82292136a115
Katherine Eaton <ktmeaton@gmail.com>
2023-05-16 19:37:32 -0600

    feature(cli): add new cli arg parser
    temporarily disable pre-commit fmt

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,28 @@
 pub mod log;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    // /// Name of the person to greet
+    // #[arg(short, long)]
+    // name: String,
+
+    // /// Number of times to greet
+    // #[arg(short, long, default_value_t = 1)]
+    // count: u8,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Download and list available datasets.
+    Dataset { name: Option<String> },
+    /// Run rebar on querry sequences.
+    Run { name: Option<String> },
+}
