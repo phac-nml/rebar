5ba3d79d27f814fdef5b8f66ae492f2f8b708cbc
Katherine Eaton <ktmeaton@gmail.com>
2023-06-05 16:36:38 -0500

    feat(cli): collapse verbosity

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,8 +1,8 @@
-pub mod verbosity;
-
 use clap::{Parser, Subcommand};
+use color_eyre::eyre::{eyre, Report, Result};
 use serde::Serialize;
 use std::path::PathBuf;
+use std::str::FromStr;
 
 #[derive(Debug, Parser, Serialize)]
 #[command(author, version, about, long_about = None)]
@@ -76,4 +76,32 @@ pub enum Command {
         #[arg(short, long, default_value_t=String::from("info"))]
         verbosity: String,
     },
+}
+
+#[derive(Debug)]
+pub enum Verbosity {
+    Info,
+    Warn,
+    Debug,
+}
+
+impl std::fmt::Display for Verbosity {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // Convert to lowercase for RUST_LOG env var compatibility
+        let lowercase = format!("{:?}", self).to_lowercase();
+        write!(f, "{lowercase}")
+    }
+}
+
+impl FromStr for Verbosity {
+    type Err = Report;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "debug" => Ok(Verbosity::Debug),
+            "info" => Ok(Verbosity::Info),
+            "warn" => Ok(Verbosity::Warn),
+            _ => Err(eyre!("Unknown verbosity level {input}.")),
+        }
+    }
 }
