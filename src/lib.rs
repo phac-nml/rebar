f34c5ef106ef5fcbd5e687d412c6b3e6d693bb47
Katherine Eaton <ktmeaton@gmail.com>
2023-07-13 16:18:10 -0600

    feat(lib): add multithreading and progress bars

--- src/lib.rs
+++ src/lib.rs
@@ -8,22 +8,49 @@ pub mod recombination;
 pub mod sequence;
 pub mod utils;
 
+use crate::dataset::SearchResult;
+use crate::recombination::{parent_search, Recombination};
 use bio::io::fasta;
 use color_eyre::eyre::{eyre, Report, Result, WrapErr};
-use indicatif::ParallelProgressIterator;
+use indicatif::{style::ProgressStyle, ProgressBar};
 use itertools::Itertools;
 use log::{debug, info, warn};
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use rayon::prelude::*;
+//use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use serde::Serialize;
 use std::fs::create_dir_all;
 
-/// Run rebar on input alignment or dataset population
+/// Run rebar on input alignment and/or dataset population
 pub fn run(args: cli::RunArgs) -> Result<(), Report> {
-    // initialize threads for rayon parallelization
+    // check how many threads are available on the system
+    let default_thread_pool = rayon::ThreadPoolBuilder::new().build().unwrap();
+    info!(
+        "Number of threads available: {}",
+        default_thread_pool.current_num_threads()
+    );
+
+    // warn the user if they requested more than their system has available
+    // if so, default to the system threads
+    let mut num_threads = args.threads;
+    if args.threads > default_thread_pool.current_num_threads() {
+        warn!(
+            "Requested --threads {} is greater than the available threads.",
+            args.threads
+        );
+        num_threads = default_thread_pool.current_num_threads();
+    }
+
+    // configure the global thread pool
+    info!("Using {} thread(s).", num_threads);
     rayon::ThreadPoolBuilder::new()
-        .num_threads(args.threads)
+        .num_threads(num_threads)
         .build_global()
         .unwrap();
+
+    // configure progress bar style
+    let progress_bar_style = ProgressStyle::with_template(
+        "{bar:40} {pos}/{len} ({percent}%) | Sequences / Second: {per_sec} | Elapsed: {elapsed_precise} | ETA: {eta_precise}"
+    ).unwrap();
 
     // create output directory if it doesn't exist
     if !args.output_dir.exists() {
@@ -36,12 +63,16 @@ pub fn run(args: cli::RunArgs) -> Result<(), Report> {
     //   and optionally a phylogenetic representation.
     let dataset = dataset::load(&args.dataset_dir, args.mask)?;
 
-    // init a container to hold query sequences, either dataset
-    // populations or sequences from an input alignment
+    // init a container to hold query sequences, dataset
+    // populations and/or sequences from an input alignment
     let mut sequences = Vec::new();
+    // keep track of ids we've seen to remove duplicates later
+    let mut ids_seen = Vec::new();
 
     // ------------------------------------------------------------------------
     // Parse Input Populations
+
+    // this step is pretty fast, don't really need a progress bar here
 
     let populations = &args.input.populations;
     if let Some(populations) = populations {
@@ -57,20 +88,26 @@ pub fn run(args: cli::RunArgs) -> Result<(), Report> {
             if population.ends_with('*') {
                 // remove last char (wildcard) with pop
                 let parent = population[0..population.len() - 1].to_string();
-                // get descendants of all parents
+                // get descendants of this population
                 let descendants = dataset.phylogeny.get_descendants(&parent)?;
-                // add parent population and descendants
+                // add population and its descendants to our search list
                 search_populations.push(parent);
                 search_populations.extend(descendants);
             } else {
+                // no phylogenetically aware mode, just add this
+                // population to our search list
                 search_populations.push(population.to_string());
             }
         }
+
+        // de-duplicate search populations
+        let search_populations = search_populations.into_iter().unique().collect_vec();
 
         for population in search_populations {
             // check if population is in dataset
             if dataset.populations.contains_key(&population) {
                 let sequence = dataset.populations[&population].clone();
+                ids_seen.push(sequence.id.clone());
                 sequences.push(sequence);
                 debug!("Added population {population} to query sequences.");
             } else {
@@ -99,7 +136,18 @@ pub fn run(args: cli::RunArgs) -> Result<(), Report> {
                 Some(&dataset.reference),
                 args.mask,
             )?;
-            sequences.push(sequence);
+
+            // check for duplicates
+            if ids_seen.contains(&sequence.id) {
+                warn!(
+                    "Sequence {} is duplicated, retaining first one.",
+                    sequence.id
+                );
+                continue;
+            } else {
+                ids_seen.push(sequence.id.clone());
+                sequences.push(sequence);
+            }
         }
     }
 
@@ -107,34 +155,55 @@ pub fn run(args: cli::RunArgs) -> Result<(), Report> {
     // Recombination Search
 
     info!("Running recombination search.");
+
+    // this step is the slowest, use progress bar and parallel threads
+    let progress_bar = ProgressBar::new(sequences.len() as u64);
+    progress_bar.set_style(progress_bar_style);
+
+    // Search for the best matches and recombination parents for each sequence.
+    // This loop/closure is structured weirdly for rayon compatability, and the
+    // fact that we need to return multiple types of objects
+    let results: Vec<(SearchResult, Recombination)> = sequences
+        .par_iter()
+        .map(|sequence| {
+            // initialize with default results, regardless of whether our
+            // searches "succeed", we're going to return standardized data
+            // structures to build our exports upon (ex. linelist columns)
+            let mut best_match = SearchResult::default();
+            let mut recombination = Recombination::new(sequence);
+
+            // search for the best match in the dataset to this sequence
+            let search_result = dataset.search(sequence, None, None);
+            if let Ok(search_result) = search_result {
+                // use the successful search as the best_match
+                best_match = search_result;
+                // setup a search for the recombination parents
+                let parent_search_args =
+                    parent_search::Args::new(&dataset, sequence, &best_match, &args);
+                let parent_search = parent_search::search_all(parent_search_args);
+                // if the search was successful, unzip the results
+                if let Ok(parent_search) = parent_search {
+                    (_, recombination) = parent_search;
+                }
+            }
+
+            progress_bar.inc(1);
+
+            (best_match, recombination)
+        })
+        .collect();
+
+    progress_bar.finish();
+
+    // unpack our search results, should be able to do this with unzip, but I
+    // keep getting errors related to implementation of Default and Extend
+
     let mut best_matches = Vec::new();
     let mut recombinations = Vec::new();
 
-    let v: Vec<_> = (0..100000).collect();
-    let v2: Vec<_> = v
-        .par_iter()
-        .progress_count(v.len() as u64)
-        .map(|i| i + 1)
-        .collect();
-    assert_eq!(v2[0], 1);
-
-    for sequence in &sequences {
-        // find the best match in the dataset to the full sequence
-        let best_match = dataset.search(sequence, None, None)?;
-
-        // organize parameters for find_parents function
-        let parent_search_args = recombination::parent_search::Args::new(
-            &dataset,
-            sequence,
-            &best_match,
-            &args,
-        );
-        // search for all recombination parents
-        let (_parents, recombination) =
-            recombination::parent_search::search_all(parent_search_args)?;
-
-        best_matches.push(best_match);
-        recombinations.push(recombination);
+    for result in results {
+        best_matches.push(result.0);
+        recombinations.push(result.1);
     }
 
     // ------------------------------------------------------------------------
@@ -212,6 +281,10 @@ pub fn plot(args: cli::PlotArgs) -> Result<(), Report> {
 
         for result in files {
             let file_path = result?.path();
+            // error we're debugging where just a ".tsv" file is produced
+            if file_path == barcodes_dir.join(".tsv") {
+                continue;
+            }
             let file_ext = file_path.extension().ok_or_else(|| {
                 eyre!("Failed to parse file extension from {file_path:?}")
             })?;
